dbf5b4d4f5f55bb20f00411ab4955d7b44601e10
Cuong Nguyen <ctring23@gmail.com>
2017-06-17 14:07:43 -0700

    Server client communication

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/generate')
+def generate():
+    return 'generated!'
+
+
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
@@ -24,4 +29,3 @@ if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END app]
